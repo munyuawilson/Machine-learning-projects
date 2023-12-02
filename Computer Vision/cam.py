2dbeadca9e25d386961dcf13ba1b464075af5a1b
munyuawilson <wmunyua4@gmail.com>
2023-12-02 17:19:02 -0500

    cam

--- Computer Vision/cam.py
+++ Computer Vision/cam.py
@@ -1,13 +1,13 @@
 import cv2
 
-cap=cv2.VideoCapture(1)
+cap=cv2.VideoCapture(0)
 
 
 while True:
     ret,frame=cap.read()
     cv2.imshow("frame",frame)
     
-    cv2.imwrite("fff.png",frame)
+    
     
 cap.release()
 
