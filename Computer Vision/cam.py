0810430a8998d0e0dd0614473b2fe20fbf8e90d4
munyuawilson <wmunyua4@gmail.com>
2023-12-11 19:03:00 -0500

    update

--- Computer Vision/cam.py
+++ Computer Vision/cam.py
@@ -1,12 +1,12 @@
 import cv2
-
+video_writer = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (640, 480))
 cap=cv2.VideoCapture(0)
 
 
 while True:
     ret,frame=cap.read()
-    cv2.imshow("frame",frame)
-    
+    #cv2.imshow("frame",frame)
+    video_writer.write(frame)
     
     
     
